699b8fe995de4c4f29f8dfaeb60ecbbcdef0972c
PatrykRajtarowski <you@example.com>
2024-05-18 10:11:45 +0200

    changed Tkinter into Customtkinter

--- youtube.py
+++ youtube.py
@@ -1,48 +1,42 @@
 import pytube.exceptions
 from pytube import YouTube
-import tkinter
 from tkinter import *
 from tkinter import messagebox
+import customtkinter as ctk
+
 fileSizeInBytes = 0
 MaxFileSize = 0
-def Downloader():
-    def Download_full():
-        def download():
-            try:
-                if len(B1.get())>1:
-                    link =B1.get()
-                    yt= YouTube(link)
-                    title= yt.title
-                    view= yt.views
-                    ys = yt.streams.get_highest_resolution()
-                    ys.download()
-                    messagebox.showinfo("Done",f"You have downloaded {title}, which has {view} views in Youtube")
-                else:
-                    messagebox.showerror("Error","Pleace copy link")
-            except pytube.exceptions.RegexMatchError:
-                    messagebox.showerror("Error","Your link is incorrect")
-            finally:
-                    messagebox.showinfo("INFO", "THANKS FOR USING MY YOUTUBE DOWNLOADER")
 
-        window.geometry('1000x250')
-        B1=Entry(window,width=30,font=('Arial',15))
-        B1.grid(row=3,column=1,padx=20,pady=20,sticky="W")
-        L1=Label(window,text="Please Entry Your Link: ",width=25,font=('Arial',15))
-        L1.grid(row=3,column=0,sticky="W")
-        B3=Button(window,text="Download",command=download,font=("arial",25),background='green')
-        B3.grid(row=7,column=0,pady=20,padx=20)
-    window= tkinter.Tk()
-    window.geometry('520x160')
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('green')
+
+def Downloader():
+    def download():
+        try:
+            if len(B2.get())>1:
+                link =B2.get()
+                yt= YouTube(link)
+                title= yt.title
+                view= yt.views
+                ys = yt.streams.get_highest_resolution()
+                ys.download()
+                messagebox.showinfo("Done",f"You Have Downloaded {title}, Which Has {view} Views In Youtube")
+                messagebox.showinfo("INFO", "Thanks For Using My Program")
+            elif len(B2.get())<=1:
+                messagebox.showerror("Error","Pleace Pass Link")
+        except pytube.exceptions.RegexMatchError:
+                messagebox.showerror("Error","Your Link Is Incorrect")
+
+    window = ctk.CTk()
+    B2=ctk.CTkEntry(window,width=500,font=('Arial',15))
+    B2.grid(row=3,column=0,padx=20,pady=20,sticky="W")
+    L2=ctk.CTkLabel(window,text="Please Pass Your Link ",width=25,font=('Arial',30))
+    L2.grid(row=2,padx=20,pady=20,column=0,sticky="n")   
+    window.geometry('520x460')
     window.title("Youtube Downloader")
-    window.config(background='blue')
-    menubar= Menu()
-    L=Label(window,text="Welcome in my Youtube Downloader",font=('Arial',20),background='orange')
-    L.grid(row=0,column=0,padx=20,pady=20)
-    filemenu = Menu(menubar, tearoff=0, background='#ffcc99', foreground='black')
-    filemenu.add_command(label="Download full Movie",command=Download_full)
-    filemenu.add_separator()
-    filemenu.add_command(label="Quit",command=window.destroy)
-    menubar.add_cascade(label="Options",menu=filemenu)
-    window.config(menu=menubar)
+    L1=ctk.CTkLabel(window,text="Welcome in my Youtube Downloader",font=('ariel',30))
+    L1.grid(row=1,column=0,padx=20,pady=20)
+    B1=ctk.CTkButton(window,text = "DOWNLOAD",command=download,font=("ariel",30))
+    B1.grid(row=6,column=0)
     window.mainloop()
 Downloader()
